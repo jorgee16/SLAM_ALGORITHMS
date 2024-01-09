35921af121084fbb62efcf410c1617a5ac38bcf2
jorgee16 <joaojorge65@gmail.com>
2024-01-09 16:39:54 +0000

    Changing launch files

--- SC-LIO-SAM/SC-LIO-SAM/src/save_path.cpp
+++ SC-LIO-SAM/SC-LIO-SAM/src/save_path.cpp
@@ -1,5 +1,11 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
+#include <tf/LinearMath/Quaternion.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+
+#include <Eigen/Dense>
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -7,46 +13,60 @@
 #include <sstream>
 #include <stdio.h>
 
-std::ofstream ofs ("/home/joaojorge/KITTI_LIO_SLAM.txt", std::ofstream::app);
+std::ofstream ofs;
+
 void callhandler(const nav_msgs::Odometry::ConstPtr& msg)
 {
     ROS_INFO("Seq: [%d]", msg->header.seq);
     ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
     ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
 
+    // Extract translation and rotation information from the odometry message
+    tf::Quaternion quaternion;
+    tf::quaternionMsgToTF(msg->pose.pose.orientation, quaternion);
+    tf::Matrix3x3 rotation_matrix(quaternion);
+    Eigen::Matrix4f transformation_matrix = Eigen::Matrix4f::Identity();
+
+    // Cast tf::Matrix3x3 to Eigen::Matrix3f
+    Eigen::Matrix3f rotation_matrix_eigen;
+    rotation_matrix_eigen << rotation_matrix[0][0], rotation_matrix[0][1], rotation_matrix[0][2],
+                            rotation_matrix[1][0], rotation_matrix[1][1], rotation_matrix[1][2],
+                            rotation_matrix[2][0], rotation_matrix[2][1], rotation_matrix[2][2];
 
 
+    transformation_matrix.block<3, 3>(0, 0) = rotation_matrix_eigen;
+    transformation_matrix.block<3, 1>(0, 3) <<  msg->pose.pose.position.x,
+                                                msg->pose.pose.position.y,
+                                                msg->pose.pose.position.z;
     if (ofs.is_open()){
-           //store array contents to text file
-           
-        //    ofs << msg->header.stamp << " " << msg->pose.pose.position.x << " " << msg->pose.pose.position.y << " " << msg->pose.pose.position.z << " " << msg->pose.pose.orientation.x << " " << msg->pose.pose.orientation.y << " " << msg->pose.pose.orientation.z << "\n";
-        ofs << msg->header.stamp << " ";
-        ofs << msg->pose.pose.position.x << " ";
-        ofs << msg->pose.pose.position.y << " ";
-        ofs << msg->pose.pose.position.z << " ";
-        ofs << msg->pose.pose.orientation.x << " ";
-        ofs << msg->pose.pose.orientation.y << " ";
-        ofs << msg->pose.pose.orientation.z << " ";
-        ofs << msg->pose.pose.orientation.w << "\n";
+
+        // Save the transformation matrix to the file
+        // ofs << msg->header.stamp << " ";
+        ofs << transformation_matrix << "\n";
+
+        // ofs << msg->header.stamp << " ";
+        // ofs << msg->pose.pose.position.x << " ";
+        // ofs << msg->pose.pose.position.y << " ";
+        // ofs << msg->pose.pose.position.z << " ";
+        // ofs << msg->pose.pose.orientation.x << " ";
+        // ofs << msg->pose.pose.orientation.y << " ";
+        // ofs << msg->pose.pose.orientation.z << " ";
+        // ofs << msg->pose.pose.orientation.w << "\n";
     }
     else 
         printf("Unable to open file \n");
 }
 
-// void write_to_file(fstream& ofs){
-//     if (ofs.is_open()){
-//         ofs
-//     }
-
-// }
 int main(int argc, char **argv)
 {   
-    // std::ofstream ofs ("home/joaojorge/LOAM_ws/save_pose.txt", std::ofstream::app);
     ros::init(argc, argv, "save_path");
     ros::NodeHandle nh;
     ros::Subscriber Listener = nh.subscribe<nav_msgs::Odometry>("lio_sam/mapping/odometry", 100, callhandler); // //integrated_to_init
     
-    // write_to_file(ofs);
+    if(ofs.good()){
+        remove("/home/joaojorge/GEORGIA_TECH_LIO_SLAM_SEQ00.txt");
+        ofs.open("/home/joaojorge/GEORGIA_TECH_LIO_SLAM_SEQ00.txt", std::ofstream::app);
+    }
     ros::spin();
 
     return 0;
